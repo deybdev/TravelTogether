18358d6f146cb174e7b48329e61defc607e331a8
Dave <138187720+deybdev@users.noreply.github.com>
2025-02-25 13:16:37 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const navMenu = document.querySelector('.nav-menu');
+const navToggle = document.querySelector('.toggle-menu');
+const navClose = document.querySelector('.nav-close');
+
+navToggle.addEventListener('click', () => {
+  navMenu.classList.add('show-menu');
+});
+
+navClose.addEventListener('click', () => {
+  navMenu.classList.remove('show-menu');
+});
