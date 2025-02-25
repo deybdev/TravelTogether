0f84abd81ebf201f8aec40f4c25a03eb147760b1
Efren Dave Cahilig <davecahilig19@gmail.com>
2025-02-25 16:19:33 +0800

    upd

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 //MENU BUTTON FOR MOBILE DEVICES
-
 const navMenu = document.querySelector('.nav-menu');
 const navToggle = document.querySelector('.toggle-menu');
 const navClose = document.querySelector('.nav-close');
@@ -11,5 +10,23 @@ navToggle.addEventListener('click', () => {
 navClose.addEventListener('click', () => {
   navMenu.classList.remove('show-menu');
 });
+
+// SEARCH BUTTON FOR MOBILE DEVICES
+const searchButton = document.querySelector('.search-btn');
+const searchBar = document.querySelector('.nav-search');
+const searchClose = document.querySelector('.close-search');
+
+searchButton.addEventListener('click', () => {
+  if (searchBar.classList.contains('show-search')) {
+    searchBar.classList.remove('show-search');
+  } else {
+    searchBar.classList.add('show-search');
+  }
+});
+
+searchClose.addEventListener('click', () => {
+  searchBar.classList.remove('show-search');
+});
+
 
 
