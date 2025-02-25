9f77e0eb76363e2a259dc29dc44877559dcafb4e
Dave <138187720+deybdev@users.noreply.github.com>
2025-02-25 13:15:52 +0800

    Delete js directory

--- js/script.js
+++
@@ -1,7 +0,0 @@
-const navMenu = document.querySelector('.nav-menu');
-const navToggle = document.querySelector('.toggle-menu');
-const navClose = document.querySelector('.nav-close');
-
-navToggle.addEventListener('click', () => {
-  navMenu.classList.add('show-menu');
-})
